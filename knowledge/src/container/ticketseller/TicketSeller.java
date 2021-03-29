40791738b4c0b6b7c4ecfecd3d6ee03492ddfa5d
chenzifeng1 <704734862@qq.com>
2021-03-29 13:57:39 +0800

    Merge branch 'main' of D:\java\work\JavaForWork with conflicts.

--- knowledge/src/container/ticketseller/TicketSeller.java
+++ knowledge/src/container/ticketseller/TicketSeller.java
@@ -2,11 +2,10 @@ package container.ticketseller;
 
 import config.StaticValue;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -26,9 +25,18 @@ public class TicketSeller {
 
     static BlockingQueue<Ticket> queue = new ArrayBlockingQueue<Ticket>(StaticValue.ONE_HUNDRED_THOUSAND);
 
-    static {
-        for (int i = 0; i < StaticValue.TEN; i++) {
+    static Vector<Ticket> ticketVector = new Vector<>(StaticValue.ONE_HUNDRED_THOUSAND);
 
+    static Queue<Ticket> ticketQueue = new ConcurrentLinkedQueue<>();
+
+    static {
+        for (int i = 0; i < StaticValue.ONE_HUNDRED_THOUSAND; i++) {
+            //初始化车票库
+            UUID uuID = UUID.randomUUID();
+            Ticket ticket = new Ticket(uuID,0);
+            ticketList.add(ticket);
+            ticketVector.add(ticket);
+            ticketVector.add(ticket);
         }
 
     }
@@ -37,7 +45,43 @@ public class TicketSeller {
     public static void main(String[] args) {
         SellerThread[] sellerWindows = new SellerThread[10];
         AtomicInteger index = new AtomicInteger(0);
-
+        TicketSeller ticketSeller = new TicketSeller();
+        ticketSeller.test_2();
     }
+
+    /**
+     * 使用List作为车票容器，在多线程环境下会发生超卖的现象
+     * 另外也可能会出现多个窗口售卖同一张票的情况
+     */
+    private void test_1(){
+        for (int i = 0; i < StaticValue.TEN; i++) {
+            new Thread(()->{
+                while (ticketList.size()>0){
+                    System.out.println(Thread.currentThread().getName() + "售出车票：" + ticketList.remove(0).getNo());
+
+                }
+            }).start();
+        }
+    }
+
+
+    /**
+     * 这个地方也有问题，虽然使用了线程安全的容器vector，但是由于只是vector的方法加锁，整个 判断->销售 的过程没有加锁，这里会导致多个线程在size=1的时候看到size>0
+     * 因此也会发送超卖现象
+     */
+    private void test_2(){
+        AtomicInteger counter = new AtomicInteger(0);
+        for (int i = 0; i < StaticValue.TEN; i++) {
+            new Thread(() -> {
+                while (ticketVector.size()>0){
+                    counter.incrementAndGet();
+                    System.out.println(Thread.currentThread().getName() + "售出车票：" + ticketVector.remove(0).getNo());
+                }
+            }).start();
+        }
+
+        System.out.println("总共售出："+ counter.get() +"张票");
+    }
+
 
 }
