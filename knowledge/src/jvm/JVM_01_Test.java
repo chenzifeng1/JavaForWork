7b1cddc72326c037cc17f860f3d6fa56d000f6f7
chenzifeng1 <704734862@qq.com>
2021-04-23 17:03:32 +0800

    1
    1

--- knowledge/src/jvm/JVM_01_Test.java
+++ knowledge/src/jvm/JVM_01_Test.java
@@ -14,13 +14,14 @@ public class JVM_01_Test {
     public static void main(String[] args) {
         showClassLoader();
     }
-
+    ClassLoader classLoader;
 
     public static void showClassLoader(){
         System.out.println(String.class.getClassLoader() );
         System.out.println(sun.misc.Unsafe.class.getClassLoader());
 
         System.out.println(JVM_01_Test.class.getClassLoader());
+        System.out.println(JVM_01_Test.class.getClassLoader().getParent());
         System.out.println(JVM_01_Test.class.getClassLoader().getClass().getClassLoader());
     }
 }
