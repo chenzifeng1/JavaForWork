66419d98fcad1c8e4d09cdbc340f39c492c6e599
chenzifeng1 <704734862@qq.com>
2021-06-15 21:04:51 +0800

    改造为maven项目

---
+++ knowledge/src/main/java/io/RPCTest/MyRPCTest.java
@@ -0,0 +1,19 @@
+package io.RPCTest;
+
+/**
+ * @ProjectName:
+ * @ClassName: MyRPCTest
+ * @Author: czf
+ * @Description: 手撸RPC代码
+ *
+ * 2. 来回通信，连接数量，拆包
+ * 3. 动态代理，序列化，协议封装
+ * 4. 连接池
+ * 5. 想调用本地方法一样调用远程的方法 在java中就是所谓的面向interface开发
+ *
+ * @Date: 2021/6/13 11:16
+ * @Version: 1.0
+ **/
+
+public class MyRPCTest {
+}
